1abb03e0b1a4cb4f9067a804f4431a8644487374
jessicadever1 <jessicadever1@gmail.com>
2021-06-10 20:32:32 -0500

    definitions

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const helloWorld = new Vue ({
     el: '#helloVue',
     data: {
         title: "Hello, world!!!!",
-        message: "This is my first Vue template!"
+        message: "This is my first Vue template!",
+        takeAway: "Templating: A representation of how you want to structure your application",
+        takeAway1: "Benefits of using JavaScript front end framework include: Abstract away common tasks, provide time-saving shortcuts, help maintain structured and organized code, and helps you take advantage of tried and true software design patters",
+        takeAway2: "Data Binding: Establishing a connection between a piece of data and a template"
     }
 });
